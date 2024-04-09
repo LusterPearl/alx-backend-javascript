5f2fe056b4843f0f990b548c16e89cea68d0fe5b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-09 11:12:47 +0100

    100

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,26 @@
+export default function createIteratorObject(report) {
+    const departments = Object.values(report.allEmployees);
+    let index = 0;
+    let employeeIndex = 0;
+  
+    return {
+      next: function() {
+        if (employeeIndex >= departments[index].length) {
+          employeeIndex = 0;
+          index++;
+        }
+  
+        if (index < departments.length) {
+          const department = departments[index];
+          const employee = department[employeeIndex];
+          employeeIndex++;
+          return { value: employee, done: false };
+        } else {
+          return { done: true };
+        }
+      },
+      [Symbol.iterator]: function() {
+        return this;
+      }
+    };
+  }

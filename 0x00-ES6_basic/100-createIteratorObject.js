765a6b83f26ccab1618e7ce36df435ca22f22cd9
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-10 12:13:17 +0100

    fixed

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,25 +1,7 @@
-export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  let index = 0;
-  let employeeIndex = 0;
-
-  return {
-    next() {
-      if (employeeIndex >= departments[index].length) {
-        employeeIndex = 0;
-        index++;
-      }
-
-      if (index < departments.length) {
-        const department = departments[index];
-        const employee = department[employeeIndex];
-        employeeIndex++;
-        return { value: employee, done: false };
-      }
-      return { done: true };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+export default function* createIteratorObject(report) {
+  for (const department of Object.values(report)) {
+    for (const employee of Object.values(department)) {
+      yield employee;
+    }
+  }
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,30 +1,9 @@
-export default function createIteratorObject(report) {
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-  const departments = Object.values(report.allEmployees);
-
-  const iterator = {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      const department = departments[currentDepartmentIndex];
-      const employee = department[currentEmployeeIndex];
-
-      currentEmployeeIndex++;
-
-      if (currentEmployeeIndex >= department.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-      }
-
-      return { value: employee, done: false };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
-
-  return iterator;
+export default function iterateThroughObject(reportWithIterator) {
+  let employeeNames = [];
+  for (const department of Object.values(reportWithIterator)) {
+    for (const employees of Object.values(department)) {
+      employeeNames = employeeNames.concat(employees);
+    }
+  }
+  return employeeNames.join(' | ');
 }

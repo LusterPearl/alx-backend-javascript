83299b2f8b552e82933cd7440fd40cec9e174629
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-11 20:28:19 +0100

    5

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,11 @@
 import ClassRoom from './0-classroom.js';
 
-export default function initializeRooms() {
-  const rooms = [
+function initializeRooms() {
+  return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
-  return rooms;
 }
+
+export default initializeRooms;

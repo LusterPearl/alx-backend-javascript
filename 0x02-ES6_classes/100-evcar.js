27a9a906c20229e605b1a15e68d78035b641be2b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-12 11:30:49 +0000

    eslint

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,13 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range; // Removed underscore
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new Car(this.brand, this.motor, this.color); // Removed underscores
   }
 }
 

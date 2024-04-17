d42206eefd0520f9236c3a711e50592f366082fc
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-17 14:55:11 +0100

    task1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ firstName: string;
 lastName: string;
 readonly fullTimeEmployee: boolean;
 yearsOfExperience: number;
-readonlylocation: string;
+readonly location: string;
 [key: string]: any;
 }
 
@@ -20,6 +20,65 @@ function createTeacher(firstName: string, lastName: string, fullTimeEmployee: bo
 } 
 
 // Create a Teacher object
-const techer3: Teacher = createTeacher("Mary", "Doe", false, "London", ["contract", false]);
+const teacher3: Teacher = createTeacher("Mary", "Doe", false, "London", ["contract", false]);
 
-console.log(techer3);
+console.log(teacher3);
+
+// Define the Directors interface extending teacher
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Create a Director object
+const director1: Directors = {
+    firstName: 'Paul',
+    lastName: 'Peter',
+    location: 'Austraila',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(director1);
+
+//  Create print Teacher
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// Implement the printTeacher function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+
+// Test the function
+console.log(printTeacher('Paul', 'Peter'));
+
+// Define the interface for the StudentClass construcrtor
+interface StudentClassConstructor {
+    new(firstName: string, lastName: string): StudentClass;
+}
+
+// Define the interface for the StudentClass
+interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+// Implent the StudentClass
+const StudentClass: StudentClassConstructor = class implements StudentClass {
+    constructor(private firstName: string, private lastName: string) {}
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  };
+
+  // Test the class
+const student = new StudentClass('John', 'Doe');
+console.log(student.workOnHomework()); // Output: Currently working
+console.log(student.displayName());
+  

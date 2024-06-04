1b4e3a9d061e4602c867f3d36966e0e702b39b6d
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-04 10:07:15 +0100

    task1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+// display the welcome message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Read user input
+process.stdin.setEncoding('utf8');
+
+// Listen for data(user input)
+process.stdin.on('data', (input) => {
+  // remove any trailing newline character from the input
+  const name = input.trim();
+
+  // DIsplay the user name
+  console.log(`Your name is ${name}`);
+
+  // End the process
+  process.stdin.end();
+});
+
+// listen for the end event to display closing message
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});

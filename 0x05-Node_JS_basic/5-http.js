727f4aef7e3584a453d2caadd9642ca9022a460b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-04 14:36:08 +0100

    adj5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
-const { module } = require('../webpack.config');
+const { config } = require('../webpack.config');
 
-// Define the port tp listen on
+// Define the port to listen on
 const port = 1245;
 
 // Create the server
@@ -22,16 +22,17 @@ const app = http.createServer((req, res) => {
       .then(() => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the list of our students\n');
-      });
+      })
       .catch((error) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
-      }
+      });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
 
-module.exports = app; 
+module.exports = app;

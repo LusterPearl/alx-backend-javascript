7da7d6b615e26dd38ac4521e187ce00943137d72
LusterPearl <Salamioluwatosin90@gmail.com>
2024-06-06 16:09:59 +0100

    tass

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,25 +1,37 @@
-const request = require('request');
-const { expect } = require('chai');
-const app = require('./api'); // Import the app for testing
+// api.test.js
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = chai;
+const server = require('./api');  // This is your Express app
+
+chai.use(chaiHttp);
 
 describe('Index page', () => {
-  const baseUrl = 'http://localhost:7865';
+  let app;
 
   before((done) => {
-    app.listen(7865, done); // Start the server before running tests
-  });
-
-  it('Correct status code?', (done) => {
-    request.get(baseUrl, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
+    app = server.listen(7865, () => {
+      console.log('Test server running on port 7865');
       done();
     });
   });
 
-  it('Correct result?', (done) => {
-    request.get(baseUrl, (err, res, body) => {
-      expect(body).to.equal('Welcome to the payment system');
+  after((done) => {
+    app.close(() => {
+      console.log('Test server stopped');
       done();
     });
   });
+
+  it('should return status 200 and correct message for GET /', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  });
+
+  // Add other tests here
 });
